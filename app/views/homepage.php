14cbfde3c155469e8ccf384e14c14068e2ec9024
SunDi3yansyah <sundi3yansyah@gmail.com>
2015-11-10 10:04:15 +0700

    missing protocol yahoo CDN 2

--- app/views/homepage.php
+++ app/views/homepage.php
@@ -10,33 +10,33 @@
     
 
 
-<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+<link rel="stylesheet" href="<?php echo base_url('assets/css/pure/pure-min.css'); ?>">
 
 
 
 <!--[if lte IE 8]>
   
-    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/grids-responsive-old-ie-min.css">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/pure/grids-responsive-old-ie-min.css'); ?>">
   
 <![endif]-->
 <!--[if gt IE 8]><!-->
   
-    <link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/grids-responsive-min.css">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/pure/grids-responsive-min.css'); ?>">
   
 <!--<![endif]-->
 
 
 
-<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
 
 
   
     <!--[if lte IE 8]>
-        <link rel="stylesheet" href="css/marketing-old-ie.css">
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/pure/marketing-old-ie.css'); ?>">
     <![endif]-->
     <!--[if gt IE 8]><!-->
-        <link rel="stylesheet" href="<?php echo base_url('assets/css/marketing.css'); ?>">
+        <link rel="stylesheet" href="<?php echo base_url('assets/css/pure/marketing.css'); ?>">
     <!--<![endif]-->
   
 
