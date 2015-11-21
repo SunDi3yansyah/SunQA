927c7ab801588866e34c530d323bccdf6bc2f9dc
SunDi3yansyah <sundi3yansyah@gmail.com>
2015-11-21 12:33:09 +0700

    Final Project PWL sumbited 2015-11-21 ─ 12:33 ─ Sabtu
    add sql

--- pwl.sql
+++ pwl.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.14  Distrib 5.5.46-MariaDB, for debian-linux-gnu (x86_64)
 --
--- Host: localhost    Database: projectPWL
+-- Host: localhost    Database: pwl
 -- ------------------------------------------------------
 -- Server version	5.5.46-MariaDB-1~trusty-log
 
@@ -14,6 +14,32 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `pwl_session`
+--
+
+DROP TABLE IF EXISTS `pwl_session`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pwl_session` (
+  `id` varchar(40) NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `timestamp` int(10) unsigned NOT NULL DEFAULT '0',
+  `data` blob NOT NULL,
+  KEY `timestamp` (`timestamp`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pwl_session`
+--
+
+LOCK TABLES `pwl_session` WRITE;
+/*!40000 ALTER TABLE `pwl_session` DISABLE KEYS */;
+INSERT INTO `pwl_session` VALUES ('10824d5f4311d66670c42845c80997df7b34d836','127.0.0.1',1448083057,'__ci_last_regenerate|i:1448083057;'),('d3e63c8b5ec2acfefa962135a19780d8bde3001e','127.0.0.1',1448083646,'__ci_last_regenerate|i:1448083646;');
+/*!40000 ALTER TABLE `pwl_session` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -24,4 +50,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-11-10 21:58:40
+-- Dump completed on 2015-11-21 12:33:01
