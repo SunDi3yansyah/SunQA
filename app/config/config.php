b53561f0bde117b96ddea1974ea56a08bb57ad4c
SunDi3yansyah <sundi3yansyah@gmail.com>
2015-11-27 19:41:09 +0700

    Final Project PWL sumbited 2015-11-27 ─ 19:41 ─ Jumat
    missing name of module

--- app/config/config.php
+++ app/config/config.php
@@ -511,3 +511,12 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = getenv('OPENSHIFT_HAPROXY_IP') ?: '';
+
+/*
+|--------------------------------------------------------------------------
+| Modules
+|--------------------------------------------------------------------------
+*/
+$config['modules_locations'] = array(
+    APPPATH.'modular/' => '../modular/',
+);
