387cecbf4405e432afc6480a0590eaf080f8dae3
SunDi3yansyah <sundi3yansyah@gmail.com>
2015-12-28 17:57:16 +0700

    Question Answer submit 2015-12-28 ─ 17:57 ─ Senin

--- app/core/MY_Controller.php
+++ app/core/MY_Controller.php
@@ -2,11 +2,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
- * @package		Question Answer (https://github.com/SunDi3yansyah/FinalProjectPWL)
- * @author		Cahyadi Triyansyah (https://sundi3yansyah.com)
- * @version		1.0
- * @license		MIT
- * @copyright	Copyright (c) 2015 SunDi3yansyah
+ * @package 		Question Answer (https://github.com/SunDi3yansyah/FinalProjectPWL)
+ * @author 			Cahyadi Triyansyah (https://sundi3yansyah.com)
+ * @version 		1.0
+ * @license 		MIT
+ * @copyright 		Copyright (c) 2015 SunDi3yansyah
  */
 
 define('STATUS_ACTIVATED', '1');
