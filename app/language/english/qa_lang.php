9c5451cb09e51344d54e1e1245b99f1c760f8377
SunDi3yansyah <sundi3yansyah@gmail.com>
2015-12-28 22:11:24 +0700

    Question Answer submit 2015-12-28 ─ 22:11 ─ Senin

--- app/language/english/qa_lang.php
+++ app/language/english/qa_lang.php
@@ -2,11 +2,11 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 /**
- * @package		Question Answer (https://github.com/SunDi3yansyah/SunQA)
- * @author		Cahyadi Triyansyah (https://sundi3yansyah.com)
- * @version		1.0
- * @license		MIT
- * @copyright		Copyright (c) 2015 SunDi3yansyah
+ * @package     Question Answer (https://github.com/SunDi3yansyah/SunQA)
+ * @author      Cahyadi Triyansyah (https://sundi3yansyah.com)
+ * @version     0.0.1
+ * @license     MIT
+ * @copyright   Copyright (c) 2015 SunDi3yansyah
  */
 
 $lang['welcome'] = 'Welcome to ';
