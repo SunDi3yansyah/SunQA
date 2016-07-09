58ea4969c968bd9f5770dc955cf29f27ea3d6dce
Cahyadi Triyansyah <sundi3yansyah@gmail.com>
2016-07-09 14:00:56 +0700

    dir sys and app incorect

--- public/index.php
+++ public/index.php
@@ -106,7 +106,7 @@ switch (ENVIRONMENT)
  * Include the path if the folder is not in the same directory
  * as this file.
  */
-	$system_path = 'sys';
+	$system_path = '../sys';
 
 /*
  *---------------------------------------------------------------
@@ -121,7 +121,7 @@ switch (ENVIRONMENT)
  *
  * NO TRAILING SLASH!
  */
-	$application_folder = 'app';
+	$application_folder = '../app';
 
 /*
  *---------------------------------------------------------------
